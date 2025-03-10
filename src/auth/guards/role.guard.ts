c364acd3f90c66693ca06cecd1e6a50723f2e444
Abhishek <aroraabhishek732@gmail.com>
2025-03-10 18:44:11 +0530

    Role Guard implementation

---
+++ src/auth/guards/role.guard.ts
@@ -0,0 +1,28 @@
+
+import { Injectable, CanActivate, ExecutionContext, ForbiddenException } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Roles } from '../decorator/roles.decorator';
+import { GqlExecutionContext } from '@nestjs/graphql';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+    canActivate(context: ExecutionContext): boolean {
+        const requiredRoles = this.reflector.get<string[]>('roles', context.getHandler());
+        
+        if (!requiredRoles) {
+          return true; // No roles required, allow access
+        }
+    
+        const ctx = GqlExecutionContext.create(context);
+        const user = ctx.getContext().req.user;
+    
+        if (!user || !requiredRoles.includes(user.role)) {
+          throw new ForbiddenException("You do not have permission to perform this action");
+        }
+    
+        return true;
+      }
+
+}
--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -3,8 +3,15 @@ import { UserService } from './user.service';
 import { CurrentUser } from 'src/auth/decorator/current-user.decorator';
 import { Auth } from 'src/auth/decorator/auth.decorator';
 import { UserEntity } from './entities/user-entity.entity';
+import { UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/role.guard';
+
+import { Roles } from 'src/auth/decorator/roles.decorator';
+import { UserRole } from 'src/roles/enum/user-role.enum';
 
 @Resolver()
+@UseGuards(JwtAuthGuard, RolesGuard)
 export class UserResolver {
   constructor(private readonly userService: UserService) {}
 
@@ -14,9 +21,8 @@ export class UserResolver {
       return this.userService.getProfile(user);
   }
 
-
   @Query(() => [UserEntity])
-  @Auth()
+  @Roles(UserRole.ADMIN)
   async getUsers() : Promise<UserEntity[]> {
       return this.userService.getUsers();
   }
